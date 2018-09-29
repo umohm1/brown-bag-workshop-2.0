9289e0f7bfc22b8b4f6ce79c2ad9b15813b22c17
Mary <mumoh@mozilla.com>
2018-09-29 12:56:29 -0400

    Fix sass package issue, add browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
+const browserSync = require('browser-sync').create();
 
 const $ = gulpLoadPlugins();
 
 gulp.task('css', () => {
-  return gulp.src('src/scss/app.css')
+  return gulp.src('src/scss/app.scss')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
     .pipe($.sass().on('error', $.sass.logError))
@@ -12,6 +13,7 @@ gulp.task('css', () => {
     .pipe($.cssnano())
     .pipe($.sourcemaps.write('./maps'))
     .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream())
 });
 gulp.task('js', () => {
   return gulp.src('src/js/*.js')
@@ -22,9 +24,22 @@ gulp.task('js', () => {
     .pipe($.uglify())
     .pipe($.sourcemaps.write('./maps'))
     .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream())
+});
+gulp.task('serve', ['css', 'js'], () => {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('src/scss/*.scss', ['css']);
+  gulp.watch('src/js/*.js', ['js']);
+  gulp.watch('*./html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['css', 'js']);
+gulp.task('build', ['css', 'js']);
+gulp.task('default', ['serve']);
+
 
 
 
