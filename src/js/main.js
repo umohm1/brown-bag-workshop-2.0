e126860faae722d6ff793388c1451e254bed651e
Mary <mumoh@mozilla.com>
2018-10-02 12:24:43 -0400

    Make API call, additional styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-alert("Hello");
+const ACCESS_KEY = "6b500f91d3830ccdabed257d3b3ac7789acb657a9816b1532c97986a048f5b41";
+window.onload = function () {
+  const hostname = 'https://api.unsplash.com';
+  const imgContainer = document.querySelector('.js-images');
+  fetch(`${hostname}/search/photos?query=dog&orientation=landscape&per_page=9`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Client-ID ${ACCESS_KEY}`,
+    }
+  }).then(res => res.json())
+    .then(response => {
+      const { results } = response;
+      const imgGrid = results.map(image => {
+        return `<div class="col col-33"><img src="${image.urls.raw}" class="img" alt=""></div>`;
+      })
+      imgContainer.innerHTML = imgGrid.join('');
+    })
+    .catch(error => console.error('Error', error));
+}
